af943b437b0491077c449a6a78aaae8cac095b0a
Anuraj Maddhesiya <143344749+Anuraj-madd@users.noreply.github.com>
2025-04-24 19:33:26 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
       </div>
 
       <div className="text-center text-sm border-t border-[#44576D] pt-2">
-        &copy; {new Date().getFullYear()} Designed with ❤️ by Anuraj Maddhesiya.<br/> All rights reserved
+        &copy; {new Date().getFullYear()} Designed with ❤️ by Anuraj Maddhesiya<br/> All rights reserved
       </div>
     </footer>
   );
